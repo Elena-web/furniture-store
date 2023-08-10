c784905852519ddd9dd24e59260b4e25ff9b5f03
Elena-web <110896958+Elena-web@users.noreply.github.com>
2023-08-10 09:38:46 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$('.content__slick').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+$('.product__slick').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+
+$('.sale-items__slick').slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+});
+ 
